27de0a47110dbe14912c6757f4569f9905dd9d27
Groot1207 <sere8887@gmail.com>
2024-12-06 20:05:39 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = 'яблоко', 'мандарин', 1, 15, True
+print(immutable_var)
+#immutable_var[2] = 10
+#print(immutable_var)
+#кортеж не поддерживает изменение
+mutable_list = [7, 8, 'абрикос', 'банан', False]
+print(mutable_list)
+mutable_list[3] = 'ананас'
+print(mutable_list)
+
